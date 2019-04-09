b2514ccedc15295140c72da09b69b60303130eed
Tyler Welander <tyler.j.welander@hotmail.com>
2019-04-09 10:59:41 -0500

    Updated libraries and connections to databases

--- EMRSystemMMT/Screens2/ScheduleAppointment.aspx.cs
+++ EMRSystemMMT/Screens2/ScheduleAppointment.aspx.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net;
+using System.Threading.Tasks;
 using MySql.Data;
+using MySql.Web;
 using MySql.Data.MySqlClient;
 
 namespace EMRSystemMMT.Screens2
@@ -21,6 +25,20 @@ namespace EMRSystemMMT.Screens2
         protected void TimeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
         {
             DoctorList.Clear();
+            //string connection_string = "Server=undcsmysql.mysql.database.azure.com; database=Micah.j.nelson; UID=micah.j.nelson@undcsmysql; password=mnelson7275";
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "undcsmysql.mysql.database.azure.com",
+                Database = "micah_j_nelson",
+                UserID = "micah.j.nelson@undcsmysql",
+                Password = "mnelson7275",
+                SslMode = MySqlSslMode.Required,
+            };
+            using (var connection = new MySqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                
+            }
         }
     }
 }
