8df9e11b0f1ac6cd32766fab5c22d29c49c43b9f
Michelle Keller <michelle.keller.1@ndus.edu>
2019-04-17 11:59:37 -0500

    - Made View Appoinment Sort of work

--- EMRSystemMMT/Screens2/ViewAppointment.aspx.cs
+++ EMRSystemMMT/Screens2/ViewAppointment.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data;
+using MySql.Web;
+using MySql.Data.MySqlClient;
 
 namespace EMRSystemMMT.Screens2
 {
@@ -11,7 +15,37 @@ namespace EMRSystemMMT.Screens2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "undcsmysql.mysql.database.azure.com",
+                Database = "micah_j_nelson",
+                UserID = "micah.j.nelson@undcsmysql",
+                Password = "mnelson7275",
+                SslMode = MySqlSslMode.Required,
+            };
+            using (var connection = new MySqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter())
+                    {
+                        command.CommandText = "SELECT a.apt_time as Date_Time, CONCAT( s.fname, ' ', s.lname ) AS Staff, mt.name as Type, mt.description as Description, t.cost as Cost " +
+                            "FROM db455_appointments a " +
+                            "JOIN db455_staff s ON s.id = a.staff_id " +
+                            "LEFT JOIN db455_ordered_tests t ON t.patient_id = a.patient_id " +
+                            "LEFT JOIN db455_medical_tests mt ON t.test_id = mt.id " +
+                            "WHERE a.patient_id = 1 AND a.apt_time >= CURRENT_TIMESTAMP;";
+                        dataAdapter.SelectCommand = command;
+                        using (DataTable dataTable = new DataTable())
+                        {
+                            dataAdapter.Fill(dataTable);
+                            ApptGridView.DataSource = dataTable;
+                            ApptGridView.DataBind();
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- EMRSystemMMT/Screens2/ViewAppointment.aspx.designer.cs
+++ EMRSystemMMT/Screens2/ViewAppointment.aspx.designer.cs
@@ -22,12 +22,12 @@ namespace EMRSystemMMT.Screens2 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// GridView1 control.
+        /// ApptGridView control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView ApptGridView;
     }
 }
